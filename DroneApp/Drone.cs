1afb72e9f86e77e96a424c494a9e0e5f49836986
liquid.studios.seattle <liquid.studios.seattle@gmail.com>
2019-04-17 15:07:36 -0700

    Move code from Program to new Drone class

---
+++ DroneApp/Drone.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using DroneLib;
+
+namespace DroneApp
+{
+    class Drone
+    {
+        // TODO: Set the correct value here.
+        const string AWS_ACCOUNT_ID = "???";
+
+        public void DroneLand()
+        {
+            using (DroneApi api = new DroneApi())
+            {
+                api.Init();
+                api.Land();
+            }
+        }
+
+        public void DroneTakeOffAndLand()
+        {
+            Console.WriteLine("Look ma, I am flying a drone!");
+            using (DroneApi api = new DroneApi())
+            {
+                api.Init();
+                api.TakeOff();
+
+                Thread.Sleep(5000);
+
+                api.Land();
+            }
+        }
+
+        public async Task TestDroneCommandFromSqsAsync(string queueName)
+        {
+            var config = new AmazonSQSConfig()
+            {
+                RegionEndpoint = RegionEndpoint.USWest2
+            };
+
+            string queueUrl = $"https://sqs.us-west-2.amazonaws.com/{AWS_ACCOUNT_ID}/" + queueName;
+            int waitTimeMilliseconds = 20 * 1000;  // long polling to reduce lag time
+
+            SqsQueue q = new SqsQueue(config);
+
+            Console.WriteLine("---Look ma, I am getting SQS message!");
+            Console.WriteLine("QueueName: {0}", queueName);
+            Console.WriteLine("QueueUrl: {0}", queueUrl);
+            Console.WriteLine("waitTimeMilliseconds: {0}", waitTimeMilliseconds);
+
+            using (DroneApi droneApi = new DroneApi())
+            {
+                while (true)
+                {
+                    droneApi.Init();
+                    Debug("Getting Drone Commands...");
+                    var message = await q.GetMessage(queueUrl, queueName, waitTimeMilliseconds, 1);
+                    string droneCommand = GetThenDeleteMessage(message);
+                    HandleDroneCommand(droneApi, droneCommand);
+
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        private void HandleDroneCommand(DroneApi droneApi, string droneCommand)
+        {
+            switch (droneCommand)
+            {
+                case "takeoff":
+                    droneApi.TakeOff();
+                    break;
+                case "land":
+                    droneApi.Land();
+                    break;
+                default:
+                    Debug("Unknown command: " + droneCommand);
+                    break;
+            }
+        }
+
+        public string GetThenDeleteMessage(Message message)
+        {
+            if (message != null)
+            {
+                Debug("command: " + message.MessageId + " " + message.Body);
+                // todo: delete message from queue
+
+                return message.Body;
+            }
+
+            return "";
+        }
+
+        public void Debug(string s)
+        {
+            Console.WriteLine("{0}: {1}", DateTime.Now, s);
+        }
+    }
+}
--- DroneApp/Program.cs
+++ DroneApp/Program.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using Amazon;
-using Amazon.SQS;
-using Amazon.SQS.Model;
-using DroneLib;
 
 namespace DroneApp
 {
@@ -12,100 +6,23 @@ namespace DroneApp
     {
         static void Main(string[] args)
         {
-            //TestDroneCommandFromSqsAsync("DroneCommandQueue4.fifo");
-            //Console.Read();
-            //DroneLand();
+            Drone drone = new Drone();
 
-            DroneTakeOffAndLand();
-        }
+            // Example
+            drone.DroneTakeOffAndLand();
 
-        static void DroneLand()
-        {
-            using (DroneApi api = new DroneApi())
-            {
-                api.Init();
-                api.Land();
-            }
-        }
-
-        static void DroneTakeOffAndLand()
-        {
-            Console.WriteLine("Look ma, I am flying a drone!");
-            using (DroneApi api = new DroneApi())
-            {
-                api.Init();
-                api.TakeOff();
-
-                Thread.Sleep(5000);
-
-                api.Land();
-            }
-        }
-
-        static async Task TestDroneCommandFromSqsAsync(string queueName)
-        {
-            var config = new AmazonSQSConfig()
-            {
-                RegionEndpoint = RegionEndpoint.USWest2
-            };
-
-            string queueUrl = "https://sqs.us-west-2.amazonaws.com/{account#}/" + queueName;
-            int waitTimeMilliseconds = 20 * 1000;  // long polling to reduce lag time
-
-            SqsQueue q = new SqsQueue(config);
-
-            Console.WriteLine("---Look ma, I am getting SQS message!");
-            Console.WriteLine("QueueName: {0}", queueName);
-            Console.WriteLine("QueueUrl: {0}", queueUrl);
-            Console.WriteLine("waitTimeMilliseconds: {0}", waitTimeMilliseconds);
-
-            using (DroneApi droneApi = new DroneApi())
-            {
-                while (true)
-                {
-                    droneApi.Init();
-                    Debug("Getting Drone Commands ... ");
-                    var message = await q.GetMessage(queueUrl, queueName, waitTimeMilliseconds, 1);
-                    string droneCommand = GetThenDeleteMessage(message);
-                    HandleDroneCommand(droneApi, droneCommand);
-
-                    Console.WriteLine();
-                }
-            }
-        }
-
-        private static void HandleDroneCommand(DroneApi droneApi, string droneCommand)
-        {
-            switch (droneCommand)
-            {
-                case "takeoff":
-                    droneApi.TakeOff();
-                    break;
-                case "land":
-                    droneApi.Land();
-                    break;
-                default:
-                    Debug("Unknown command: " + droneCommand);
-                    break;
-            }
-        }
-
-        static string GetThenDeleteMessage(Message message)
-        {
-            if (message != null)
-            {
-                Debug("command: " + message.MessageId + " " + message.Body);
-                // todo: delete message from queue
-
-                return message.Body;
-            }
-
-            return "";
-        }
-
-        static void Debug(string s)
-        {
-            Console.WriteLine("{0}: {1}", DateTime.Now, s);
+            // Comment the above line and uncomment the below to work on the exercise for the mobbing session
+            //try
+            //{
+            //    Console.WriteLine("Starting...");
+            //    drone.TestDroneCommandFromSqsAsync("DroneCommandQueue1.fifo").Wait();
+            //    //Console.Read();
+            //}
+            //finally
+            //{
+            //    drone.DroneLand();
+            //    Console.WriteLine("Exiting");
+            //}
         }
     }
 }
